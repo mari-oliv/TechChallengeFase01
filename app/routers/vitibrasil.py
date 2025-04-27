cfef0eb014ba8d962aae277f1a7715fd13c9d34f
Pedro Costa <pedrocosta2802@gmail.com>
2025-04-27 20:10:42 -0300

    One last scarper for the proc page and 2 new endpoints

--- app/routers/vitibrasil.py
+++ app/routers/vitibrasil.py
@@ -39,7 +39,8 @@ def parse_float(value: Optional[str]) -> Optional[float]:
     except ValueError:
         return None
 
-#rota da aba processamento, subselção viniferas
+
+#rota da aba processamento, subseleção viniferas
 @router.get("/processamento/viniferas")
 async def get_viniferas(
     year: int = Query(None, ge=1970, le=2023),
@@ -88,3 +89,111 @@ async def get_viniferas(
     
     except Exception as e:
         return {"success": False, "error": str(e)}
+
+
+#rota da aba processamento, subseleção americanas e hibridas
+@router.get("/processamento/viniferas/americanas&hibridas")
+async def get_viniferas(
+    year: int = Query(None, ge=1970, le=2023),
+    group: str = Query(None),
+    cultive: str = Query(None),
+    quant_min: Optional[str] = Query(None),
+    quant_max: Optional[str] = Query(None)
+):
+    try:
+        quant_min_value = parse_float(quant_min)
+        quant_max_value = parse_float(quant_max)
+
+        conn = sqlite3.connect("vitibrasil_proc.db")
+        cursor = conn.cursor()
+
+        query = "SELECT Year, GroupName, Cultive, Quantity_Kg FROM proc_viniferas_ame_hib WHERE 1=1" #query inicial
+        params = [] #armazena parametros da query
+        if year is not None:
+            query += " AND Year = ?"
+            params.append(year) #adiciona ano a lista dos parametros criada
+
+        if group:
+            query += " AND GroupName LIKE ?"
+            params.append(f"%{group}%") #adiciona grupo a lista dos parametros criada, utilizando o LIKE para nao precisar ser identico
+
+        if cultive:
+            query += " AND Cultive LIKE ?"
+            params.append(f"%{cultive}%")
+
+        if quant_min_value is not None:
+            query += " AND CAST(REPLACE(Quantity_Kg, '.', '') AS REAL) >= ?"
+            params.append(quant_min)
+
+        if quant_max_value is not None:
+            query += " AND CAST(REPLACE(Quantity_Kg, '.', '') AS REAL) <= ?"
+            params.append(quant_max)
+            
+        
+        cursor.execute(query, params)#monta a query e executa os filtros caso sejam passados
+        rows = cursor.fetchall() #pega todos os dados da query
+
+        data = [{"Year": row[0], "GroupName": row[1], "Cultive": row[2], "Quantity_Kg": row[3]} for row in rows]
+        conn.close() #fecha conexao
+        
+        return {"success": True, "total": len(data), "data": data}
+    
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+    
+
+#rota da aba processamento, subseleção uvas de mesa
+@router.get("/processamento/viniferas/uvas_de_mesa")
+async def get_viniferas(
+    year: int = Query(None, ge=1970, le=2023),
+    group: str = Query(None),
+    cultive: str = Query(None),
+    quant_min: Optional[str] = Query(None),
+    quant_max: Optional[str] = Query(None)
+):
+    try:
+        quant_min_value = parse_float(quant_min)
+        quant_max_value = parse_float(quant_max)
+
+        conn = sqlite3.connect("vitibrasil_proc.db")
+        cursor = conn.cursor()
+
+        query = "SELECT Year, GroupName, Cultive, Quantity_Kg FROM proc_viniferas_uvas_mesa WHERE 1=1" #query inicial
+        params = [] #armazena parametros da query
+        if year is not None:
+            query += " AND Year = ?"
+            params.append(year) #adiciona ano a lista dos parametros criada
+
+        if group:
+            query += " AND GroupName LIKE ?"
+            params.append(f"%{group}%") #adiciona grupo a lista dos parametros criada, utilizando o LIKE para nao precisar ser identico
+
+        if cultive:
+            query += " AND Cultive LIKE ?"
+            params.append(f"%{cultive}%")
+
+        if quant_min_value is not None:
+            query += " AND CAST(REPLACE(Quantity_Kg, '.', '') AS REAL) >= ?"
+            params.append(quant_min)
+
+        if quant_max_value is not None:
+            query += " AND CAST(REPLACE(Quantity_Kg, '.', '') AS REAL) <= ?"
+            params.append(quant_max)
+            
+        
+        cursor.execute(query, params)#monta a query e executa os filtros caso sejam passados
+        rows = cursor.fetchall() #pega todos os dados da query
+
+        data = [{"Year": row[0], "GroupName": row[1], "Cultive": row[2], "Quantity_Kg": row[3]} for row in rows]
+        conn.close() #fecha conexao
+        
+        return {"success": True, "total": len(data), "data": data}
+    
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+    
+
+
+
+    
+
