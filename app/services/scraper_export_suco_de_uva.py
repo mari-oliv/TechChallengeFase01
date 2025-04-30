354fadc59f441f266dfcbf6d0fcabe066e6e93af
Pedro Costa <pedrocosta2802@gmail.com>
2025-04-30 16:21:26 -0300

    Another scraper and API route this time for 'suco_de_uva'

---
+++ app/services/scraper_export_suco_de_uva.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3
+
+def get_export_suco_uva(year: int) -> pd.DataFrame:
+    URL = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={year}&opcao=opt_06&subopcao=subopt_04" #monta url dinamica
+    response = requests.get(URL)
+    response.encoding ='utf-8'
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    table = soup.find("table", class_="tb_base tb_dados")
+    if not table: #se nao encontrar nao teve prod esse ano 'vazio'
+        return pd.DataFrame()
+    
+    rows = table.find_all("tr")
+    data = [] #armazena data
+
+    for row in rows:
+        cols = row.find_all("td")
+        if len(cols) != 3:
+            continue
+
+        country = cols[0].text.strip()
+        quantity = cols[1].text.strip()
+        value = cols[2].text.strip()
+
+        data.append({
+            "Year": year,
+            "Country": country, 
+            "Quantity_Kg": quantity, 
+            "Value_USD": value
+        })
+
+    return pd.DataFrame(data) #salva dados
+
+def save_at_db_exportacao(df: pd.DataFrame) -> None:
+    conn = sqlite3.connect("vitibrasil_export.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS exportacao_suco_de_uva (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Year INTEGER,
+            Country TEXT,
+            Quantity_Kg TEXT, 
+            Value_USD TEXT
+        )
+    ''')#cria a table
+
+    df.to_sql("exportacao_uvas_frescas", conn, if_exists="append", index=False)#dataframe para sql
+
+    conn.commit()
+    conn.close()
+    print("All data saved")
+
+
+def export_all_years_exportacao():
+    
+    for year in range(1970, 2025):
+        print(f"Extracting data year: {year}")
+        df = get_export_suco_uva(year) #chama a function para cada ano 70's - 2024
+        if not df.empty:
+            save_at_db_exportacao(df)
+    
+    
+if __name__ == "__main__":
+    export_all_years_exportacao()
